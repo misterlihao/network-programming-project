231c1f045a9b534f1aaf5e016e063a687c2d9402
misterlihao <misterlihao@gmail.com>
2015-05-09 15:36:30 +0800

    make FriendList in friend_machanism.py iterable

--- friend_mechanism.py
+++ friend_mechanism.py
@@ -2,22 +2,22 @@ from online_check import *
 class FriendList:
     def __init__(self, file_name):
         self.file_name = file_name
-        self.friends_status_list = []
+        self.ip_name_status_list = []
         with open(self.file_name) as file:
             for line in file:
                 ip, name = line.split(':')
-                self.friends_status_list.append([ip, name, 'Off'])#default to offline
+                self.ip_name_status_list.append([ip, name, 'Off'])#default to offline
                 
     def Save(self):
         with open(self.file_name, 'w') as file:
-            for each in self.friends_status_list:
+            for each in self.ip_name_status_list:
                 file.write(each[0]+':'+each[1])
         
     def RefreshOnlineStatus(self):
         '''
         refresh online status
         '''
-        for each in self.friends_status_list:
+        for each in self.ip_name_status_list:
             if CheckSomeoneOnline(each[0]) == True:
                 each[2] = 'On'
             elif CheckSomeoneOnline(each[0]) == False:
@@ -26,6 +26,12 @@ class FriendList:
     def AddNewFriend(self,ip, name):
         '''
         '''
-        self.friends_status_list.append([ip, name, 'Off'])
+        self.ip_name_status_list.append([ip, name, 'Off'])
         
         pass
+    def __iter__(self):
+        '''
+        iteration of [ip, name, status]
+        '''
+        for item in self.ip_name_status_list:
+            yield item
