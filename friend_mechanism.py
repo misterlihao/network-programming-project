9f3fbaa50775a59391cf58b5e7b2cff28196d446
misterlihao <misterlihao@gmail.com>
2015-05-09 15:31:08 +0800

    make friend list in friend_mechanism.py an object

--- friend_mechanism.py
+++ friend_mechanism.py
@@ -1,28 +1,31 @@
-import sys
+from online_check import *
+class FriendList:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.friends_status_list = []
+        with open(self.file_name) as file:
+            for line in file:
+                ip, name = line.split(':')
+                self.friends_status_list.append([ip, name, 'Off'])#default to offline
+                
+    def Save(self):
+        with open(self.file_name, 'w') as file:
+            for each in self.friends_status_list:
+                file.write(each[0]+':'+each[1])
+        
+    def RefreshOnlineStatus(self):
+        '''
+        refresh online status
+        '''
+        for each in self.friends_status_list:
+            if CheckSomeoneOnline(each[0]) == True:
+                each[2] = 'On'
+            elif CheckSomeoneOnline(each[0]) == False:
+                each[2] = 'Off'
 
-def ReadFriendList(file_name):
-    friends_status_list = []
-    with open(file_name) as file:
-        for line in file:
-            ip, name = line.split(':')
-            friends_status_list.append((ip, name, 'Off'))#default to offline
-            print(friends_status_list)
-        return friends_status_list
-            
-def WriteFriendList(file_name, friends_status_list):
-    with open(file_name, 'w') as file:
-        for each in friends_status_list:
-            file.write(each[0]+':'+each[1])
-            
-def RefreshOnlineFriends(friends_status_list):
-    for each in friends_status_list:
-        if CheckSomeoneOnline(each[0]) == True:
-            refreshed_list.append((each[0], each[1], 'On'))
-        elif CheckSomeoneOnline(each[0]) == False:
-            refreshed_list.append((each[0], each[1], 'Off'))
-    return refreshed_list
-
-def AddNewFriend(friends_status_list, ip, name):
-    friends_status_list.append((ip, name, 'Off'))
-    RefreshOnlineFriends(friends_status_list)
-    pass
+    def AddNewFriend(self,ip, name):
+        '''
+        '''
+        self.friends_status_list.append([ip, name, 'Off'])
+        
+        pass
